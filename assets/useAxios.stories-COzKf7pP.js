91bb97b7de356f8e29fa6a820ba939430afe5f28
Fullsize <1619686166@qq.com>
2024-03-26 08:56:23 +0000

    Deploying to gh-pages from @ Fullsize/react-hooks-library@26b8e8248891d6c528d1c00398da70ba1a9c182b 🚀

---
+++ assets/useAxios.stories-COzKf7pP.js
@@ -0,0 +1,5 @@
+import{R as e}from"./index-BBkUAzwr.js";import{S as p,u as i}from"./ShowDocs-CJznU43B.js";const d=()=>{const[{data:g,loading:l,error:u}]=i({url:"/myapi"});return e.createElement(e.Fragment,null,l&&e.createElement(e.Fragment,null,"...."),u&&e.createElement(e.Fragment,null,"请求错误"))},E={title:"base/useAxios",component:d},r={render:()=>e.createElement(p,{md:"doc/useAxios.md"})},o={render:()=>e.createElement(d,null)};var a,s,t;r.parameters={...r.parameters,docs:{...(a=r.parameters)==null?void 0:a.docs,source:{originalSource:`{
+  render: () => <ShowDocs md={"doc/useAxios.md"} />
+}`,...(t=(s=r.parameters)==null?void 0:s.docs)==null?void 0:t.source}}};var n,c,m;o.parameters={...o.parameters,docs:{...(n=o.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  render: () => <Page />
+}`,...(m=(c=o.parameters)==null?void 0:c.docs)==null?void 0:m.source}}};const S=["Doc","Demo"];export{o as Demo,r as Doc,S as __namedExportsOrder,E as default};
---
+++ doc/useAxios.md
@@ -0,0 +1,19 @@
+# useAxios
+
+基于axios的react hooks 封装，使用方法如axios一样
+
+```javascript
+import React from "react";
+import { useAxios } from "../src/index";
+const Page = () => {
+  const [{ data, loading, error }] = useAxios({
+    url: "/myapi",
+    method: "post",
+    data: {
+      // my data
+    },
+  });
+  return <></>;
+};
+export default Page;
+```
