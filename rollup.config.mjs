691d5da8d0e1b87159eabbf16f3671ea22fd307d
无涯 <1619686166@qq.com>
2024-03-27 10:45:07 +0800

    feat: dist

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -4,8 +4,8 @@ import typescript from '@rollup/plugin-typescript';
 export default {
   input: 'src/index.ts', // 入口文件路径
   output: {
-    file: './index.js', // 输出文件路径
-    format: 'cjs', // 输出模块格式为 CommonJS
+    file: 'dist/index.js', // 输出文件路径
+    format: 'umd', // 输出模块格式为 CommonJS
     sourcemap: false, // 生成 source map
   },
   plugins: [
