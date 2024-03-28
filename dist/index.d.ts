08ec41fe3a907054db9e26fda999a53864004ee7
无涯 <1619686166@qq.com>
2024-03-28 18:08:20 +0800

    feat: 更新依赖

--- dist/index.d.ts
+++
@@ -1,3 +0,0 @@
-import useAxios from './useAxios';
-import useLocalStorage from './useLocalStorage';
-export { useAxios, useLocalStorage };
--- dist/useLocalStorage.d.ts
+++
@@ -1,2 +0,0 @@
-declare function useLocalStorage<T>(key: string, initialValue?: T): [value: T | any, handle: (data: T | any) => void, remove: () => void];
-export default useLocalStorage;
