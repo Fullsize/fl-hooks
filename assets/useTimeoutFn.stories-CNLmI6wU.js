868ba1e60a8cac8a90a455f7d00397495d8b35dc
Fullsize <1619686166@qq.com>
2024-07-09 07:07:46 +0000

    Deploying to gh-pages from @ Fullsize/react-hooks-library@b33be984abe2dfef93eebc8aca660cd3dced41b6 🚀

--- assets/useTimeoutFn.stories-CNLmI6wU.js
+++
@@ -1,5 +0,0 @@
-import{r as l,R as r}from"./index-BBkUAzwr.js";import{S as f}from"./ShowDocs-NmJmU2e-.js";import{u as D}from"./useTimeout-DYcvUwZs.js";function F(t,p=0){const[s]=D(t,p);return l.useEffect(()=>{s()},[]),{resetTimeout:s}}const i=()=>(F(()=>{console.log("Delayed function executed!")},1e3),r.createElement(r.Fragment,null)),S={title:"base/useTimeoutFn",component:i},e={render:()=>r.createElement(f,{md:"doc/useTimeoutFn.md"})},o={render:()=>r.createElement(i,null)};var n,c,a;e.parameters={...e.parameters,docs:{...(n=e.parameters)==null?void 0:n.docs,source:{originalSource:`{
-  render: () => <ShowDocs md={"doc/useTimeoutFn.md"} />
-}`,...(a=(c=e.parameters)==null?void 0:c.docs)==null?void 0:a.source}}};var m,u,d;o.parameters={...o.parameters,docs:{...(m=o.parameters)==null?void 0:m.docs,source:{originalSource:`{
-  render: () => <Page />
-}`,...(d=(u=o.parameters)==null?void 0:u.docs)==null?void 0:d.source}}};const x=["Doc","Demo"];export{o as Demo,e as Doc,x as __namedExportsOrder,S as default};
