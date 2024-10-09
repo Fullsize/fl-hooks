0f7d85fc64b42f281c4e3578bba4b8e29d1458b1
Fullsize <1619686166@qq.com>
2024-10-09 03:13:43 +0000

    Deploying to gh-pages from @ Fullsize/fl-hooks@e06416d5a1a86a839d034183dc139a7d24e0d8ab 🚀

---
+++ assets/useIsClient.stories-DOH1VMjp.js
@@ -0,0 +1,5 @@
+import{r as o,R as t}from"./index-uubelm5h.js";import{S as p}from"./ShowDocs-CEFBxudd.js";import"./client-D4tOc2ZC.js";function f(){const[s,u]=o.useState(!1);return o.useEffect(()=>{u(!0)},[]),s}const l=()=>{const s=f();return t.createElement("div",null,"是否在客户端?",s?"是":"否")},S={title:"base/useIsClient",component:l},e={render:()=>t.createElement(p,{md:"doc/useIsClient.md"})},r={render:()=>t.createElement(l,null)};var n,a,c;e.parameters={...e.parameters,docs:{...(n=e.parameters)==null?void 0:n.docs,source:{originalSource:`{
+  render: () => <ShowDocs md={"doc/useIsClient.md"} />
+}`,...(c=(a=e.parameters)==null?void 0:a.docs)==null?void 0:c.source}}};var m,d,i;r.parameters={...r.parameters,docs:{...(m=r.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  render: () => <Page />
+}`,...(i=(d=r.parameters)==null?void 0:d.docs)==null?void 0:i.source}}};const I=["Doc","Demo"];export{r as Demo,e as Doc,I as __namedExportsOrder,S as default};
---
+++ assets/useToggle.stories-DvPQviN0.js
@@ -0,0 +1,5 @@
+import{r as c,R as e}from"./index-uubelm5h.js";import{S as f}from"./ShowDocs-CEFBxudd.js";import"./client-D4tOc2ZC.js";function D(s){const[r,a]=c.useState(s),E=c.useCallback(n=>{a(n!==void 0?n:!r)},[r]);return[r,E]}const i=()=>{const[s,r]=D(!1);return e.createElement(e.Fragment,null,e.createElement("span",null,"当前状态: ",s?"开":"关"),e.createElement("button",{onClick:()=>r()},"操作"))},x={title:"base/useToggle",component:i},o={render:()=>e.createElement(f,{md:"doc/useToggle.md"})},t={render:()=>e.createElement(i,null)};var m,l,u;o.parameters={...o.parameters,docs:{...(m=o.parameters)==null?void 0:m.docs,source:{originalSource:`{
+  render: () => <ShowDocs md={"doc/useToggle.md"} />
+}`,...(u=(l=o.parameters)==null?void 0:l.docs)==null?void 0:u.source}}};var d,g,p;t.parameters={...t.parameters,docs:{...(d=t.parameters)==null?void 0:d.docs,source:{originalSource:`{
+  render: () => <Page />
+}`,...(p=(g=t.parameters)==null?void 0:g.docs)==null?void 0:p.source}}};const h=["Doc","Demo"];export{t as Demo,o as Doc,h as __namedExportsOrder,x as default};
